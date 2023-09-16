7a26d179f66d1ffc7c143aaec9dce107e74971ae
Papaw Fenrir <papawfen@in-d1.nsk.21-school.ru>
2023-09-16 16:30:37 +0700

    demo version server

---
+++ grpc_serv/src/demo_client.py
@@ -0,0 +1,15 @@
+import os
+import lib
+
+if __name__ == '__main__':
+    client = lib.FileClient('localhost:8888')
+
+    # # demo for file uploading
+    # in_file_name = 'aboba.png'
+    # client.upload(in_file_name)
+
+    # demo for file downloading:
+    out_file_name = 'aboba.png'
+    if os.path.exists(out_file_name):
+        os.remove(out_file_name)
+    client.download('whatever_name', out_file_name)
