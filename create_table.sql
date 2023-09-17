15cc7c38b75c08f2d4cc27d773e32428b7ce95ac
Pearle Craft <pearlecr@in-d5.nsk.21-school.ru>
2023-09-17 10:58:03 +0700

    db ready

--- create_table.sql
+++ create_table.sql
@@ -1,44 +1,90 @@
 ---
-drop table Users cascade;
+create schema public;
 Drop table Groups cascade;
 Drop table Files cascade;
+drop table Users cascade;
+Drop function fnc_trg();
 CREATE TABLE Users (
     userid UUID PRIMARY KEY,
     userroot VARCHAR NOT NULL unique
 );
+CREATE TABLE Files (
+    groupid BIGINT PRIMARY KEY,
+    userid UUID REFERENCES Users (userid),
+    filepath VARCHAR NOT NULL,
+    unique (userid, filepath)
+);
 CREATE TABLE Groups (
-    usergroup BIGINT PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
+    groupid BIGINT REFERENCES Files (groupid),
     userid UUID REFERENCES Users(userid),
     accesslevel INTEGER
 );
-CREATE TABLE Files (
-    fileid BIGINT PRIMARY KEY,
-    filepath VARCHAR NOT NULL,
-    userid UUID REFERENCES Users (userid),
-    usergroup BIGINT REFERENCES Groups (usergroup)
-);
+---
+CREATE OR REPLACE FUNCTION fnc_trg() RETURNS trigger AS $$ BEGIN
+INSERT INTO Groups
+VALUES (
+        (
+            SELECT coalesce(MAX(id), 0) + 1
+            FROM Groups
+        ),
+        NEW.groupid,
+        NEW.userid,
+        0
+    );
+RETURN NEW;
+END;
+$$ language plpgsql;
+---
+CREATE TRIGGER tr_file
+AFTER
+INSERT ON Files FOR EACH ROW EXECUTE FUNCTION fnc_trg();
 ---
 INSERT INTO Users
 VALUES (
         'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-        '/test/test1.txt'
+        '/test1/'
     );
-INSERT INTO Groups
+INSERT INTO Users
 VALUES (
-        (
-            SELECT COUNT(*) + 1
-            FROM Groups
-        ),
-        'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-        1
+        '203caf80-1ee7-4246-8187-a148c3ecc61b',
+        '/test2/'
     );
 INSERT INTO Files
 VALUES (
         (
-            SELECT COUNT(*) + 1
+            SELECT coalesce(MAX(groupid), 0) + 1
             FROM Files
         ),
-        '/fjsdkv/kljvbls.erh',
         'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+        '/test2/test.txt'
+    );
+INSERT INTO Groups
+VALUES (
+        (
+            SELECT coalesce(MAX(id), 0) + 1
+            FROM Groups
+        ),
+        (
+            SELECT groupid
+            FROM Files
+            WHERE filepath = '/test2/test.txt'
+                AND userid = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'
+        ),
+        '203caf80-1ee7-4246-8187-a148c3ecc61b',
         1
     );
+---
+SELECT userid
+FROM users
+WHERE userid = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11';
+--
+SELECT filepath FROM files WHERE userid = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11';
+--
+SELECT userid, accesslevel FROM groups WHERE groupid =
+(
+    SELECT groupid
+    FROM files
+    WHERE filepath = '/test2/test.txt'
+        AND userid = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'
+);
